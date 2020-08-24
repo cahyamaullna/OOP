7767679c544f97e24994b8a7eaccb22635c13dec
muhammad cahya maulana <56526152+cahyamaullna@users.noreply.github.com>
2020-08-25 00:19:31 +0700

    OOP
    Belajar class and object di PHP

---
+++ belajar oop/belajarphp1.php
@@ -0,0 +1,9 @@
+<?php
+$nama = "Muhammad Cahya Maulana";
+$rombel = "RPL XI-3";
+echo $nama;
+echo"<br/>";
+echo $rombel;
+echo"<br/>";
+echo "hai, Perkenalkan Nama Saya ".$nama." saya dari rombel ".$rombel." saya belajar oop di php";
+?>
